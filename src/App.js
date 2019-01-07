7e307d9c1631cb84769e77ce9dc32c3fc4d81fa3
Bhanu <pbhanu.1994@gmail.com>
2019-01-07 19:13:56 +1100

    Latest commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,99 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
+  state = {
+    text: {
+      recepient: "",
+      message: ""
+    }
+  };
+  styles = {
+    forms: {
+      marginTop: 200,
+      marginLeft: 400
+    },
+    message: {
+      marginTop: 20
+      // maxWidth: 500
+    }
+  };
+
+  sendMessage = () => {
+    const { text } = this.state;
+    console.log("Phone: ", text.recepient, "Message: ", text.message);
+
+    //Pass the variables within the query string
+    fetch(
+      `http://127.0.0.1:4000/send-text?recepient=${
+        text.recepient
+      }&textMessage=${text.message}`
+    ).catch(err => console.error(err));
+  };
+
   render() {
+    const { text } = this.state;
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <React.Fragment>
+        <div className="container">
+          <center>
+            <h3 style={{ paddingTop: 50 }}>
+              Hello there, send the MMS here..!! :)
+            </h3>
+          </center>
+          <div className="form" style={this.styles.forms}>
+            <div>
+              <label for="exampleFormControlFile1">Example file input</label>
+              <input type="file" class="form-control-file" />
+            </div>
+            <div className="row">
+              <input
+                className="form-control form-control-lg col-2"
+                type="text"
+                placeholder="+61"
+                disabled
+              />
+              <input
+                value={text.recepient}
+                onChange={e =>
+                  this.setState({
+                    text: { ...text, recepient: e.target.value }
+                  })
+                }
+                className="form-control form-control-lg col-4"
+                type="text"
+                placeholder="444 444 444"
+                required
+              />
+            </div>
+            <div className="row">
+              <input
+                value={text.message}
+                onChange={e =>
+                  this.setState({
+                    text: { ...text, message: e.target.value }
+                  })
+                }
+                style={this.styles.message}
+                className="form-control form-control-lg col-6 form"
+                type="text"
+                placeholder="Type your Message here..."
+                required
+              />
+            </div>
+            <div className="row">
+              <button
+                onClick={this.sendMessage}
+                style={this.styles.message}
+                type="submit"
+                className="form-control btn btn-primary btn-lg btn-block col-6"
+              >
+                Send Message
+              </button>
+            </div>
+          </div>
+        </div>
+      </React.Fragment>
     );
   }
 }
